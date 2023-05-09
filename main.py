3c74f31f5f9e2ef4a273ba6bd5a784b86d26d7fc
HooradAboo <hoorad.abootalebi@gmail.com>
2023-05-09 15:18:03 -0400

    add recieved data to MongoDB

--- main.py
+++ main.py
@@ -4,18 +4,28 @@
     lsof -i :8000
     kill -9 
 '''
-
 from fastapi import Request, FastAPI
+from pymongo import MongoClient
+from  json import loads
 
 app = FastAPI()
+
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://hoorad:h8rad@cluster0.qm6fhqa.mongodb.net/?retryWrites=true&w=majority")  # Replace <mongo-db-url> with your MongoDB connection URL
+db = client["U2M"]  # Replace "mydatabase" with the name of your MongoDB database
+collection = db["sensor"]  # Replace "mycollection" with the name of your MongoDB collection
 
 @app.post("/")
 async def get_body(request: Request):
     if request.body:
         body_bytes = await request.body()
         body_str = body_bytes.decode("utf-8")
-        print(body_str)
+        body_dict = loads(body_str)
+
+        # Save the data to MongoDB
+        collection.insert_one(body_dict)
+
         return body_str
     else:
         return "No request body found"
-    
